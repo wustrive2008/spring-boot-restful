560744f9081bd7b83e209775aa3d074027aeec74
wubaoguo <wustrive_2008@126.com>
2017-02-19 20:07:23 +0800

    swagger2实现api文档

--- src/main/java/com/wubaoguo/springboot/bean/User.java
+++ src/main/java/com/wubaoguo/springboot/bean/User.java
@@ -1,18 +1,19 @@
 package com.wubaoguo.springboot.bean;
 
 public class User {
-	private Long id;  
+	private String id;  
     private String name;  
   
-    public Long getId() {  
-        return id;  
-    }  
   
-    public void setId(Long id) {  
-        this.id = id;  
-    }  
-  
-    public String getName() {  
+    public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {  
         return name;  
     }  
   
