b30d613ca7b11b999bd790b289afe0540c94133b
wubaoguo <wustrive_2008@126.com>
2017-02-20 00:32:52 +0800

    添加异步任务

--- src/main/java/com/wubaoguo/springboot/Application.java
+++ src/main/java/com/wubaoguo/springboot/Application.java
@@ -2,21 +2,15 @@ package com.wubaoguo.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @SpringBootApplication 
 @EnableScheduling
+@EnableAsync
 public class Application {
-	
-	@RequestMapping("/")
-	@ResponseBody
-    String home() {
-        return "Hello World!";
-    }
 	
 	public static void main(String[] args){  
         SpringApplication.run(Application.class, args);
