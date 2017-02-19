b4540a7a6126c064bbcf504dd3ab3d8ee88be875
wubaoguo <wustrive_2008@126.com>
2017-02-19 16:10:09 +0800

    helloworld

---
+++ src/main/java/com/wubaoguo/springboot/Application.java
@@ -0,0 +1,22 @@
+package com.wubaoguo.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication 
+public class Application {
+	
+	@RequestMapping("/")
+	@ResponseBody
+    String home() {
+        return "Hello World!";
+    }
+	
+	public static void main(String[] args){  
+        SpringApplication.run(Application.class, args);
+    }  
+}
