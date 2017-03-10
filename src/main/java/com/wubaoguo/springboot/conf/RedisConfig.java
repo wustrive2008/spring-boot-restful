9fe96e44bed004c6ff2371f7789e27f7499b85e5
wubaoguo <wustrive_2008@126.com>
2017-03-11 00:31:15 +0800

    解决全局redis与shiro redis冲突问题

--- src/main/java/com/wubaoguo/springboot/conf/RedisConfig.java
+++ src/main/java/com/wubaoguo/springboot/conf/RedisConfig.java
@@ -1,35 +1,44 @@
 package com.wubaoguo.springboot.conf;
 
+import org.springframework.cache.CacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
-import com.fasterxml.jackson.annotation.PropertyAccessor;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.wubaoguo.springboot.redis.RedisObjectSerializer;
 
-//@Configuration
+@Configuration
 public class RedisConfig {
-    
-    @Bean  
-    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory factory) {  
-        StringRedisTemplate template = new StringRedisTemplate(factory);  
-        // 设置序列化工具， 这样缓存的Bean就不需要再试下Serializable接口  
-        setSerrializer(template);  
-        template.afterPropertiesSet();  
-        return template;  
-    }  
-  
-    // 设置序列化  
-    private void setSerrializer(StringRedisTemplate template) {  
-        Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);  
-        ObjectMapper om = new ObjectMapper();  
-        om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);  
-        om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);  
-        jackson2JsonRedisSerializer.setObjectMapper(om);  
-        template.setValueSerializer(jackson2JsonRedisSerializer);  
-    }  
+	
+	/**
+	 * 当 redis 与shiro redis共存时
+	* @Title: cacheManager 
+	* @Description: TODO(这里用一句话描述这个方法的作用) 
+	* @param  redisTemplate
+	* @param  参数说明 
+	* @return CacheManager    返回类型 
+	* @throws
+	 */
+	@Bean
+	public CacheManager cacheManager(RedisTemplate<String, Object> redisTemplate) {
+		CacheManager cacheManager = new RedisCacheManager(redisTemplate);
+		return cacheManager;
+	}
+	 
+	@Bean
+    JedisConnectionFactory jedisConnectionFactory() {
+        return new JedisConnectionFactory();
+    }
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<String, Object>();
+        template.setConnectionFactory(jedisConnectionFactory());
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new RedisObjectSerializer());
+        return template;
+    }
 }
