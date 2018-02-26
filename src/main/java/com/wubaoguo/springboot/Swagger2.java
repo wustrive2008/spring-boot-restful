3e04c73d71007d18d1448eda3b89c93b2fd059f3
wubaoguo <wustrive_2008@126.com>
2018-02-26 11:24:37 +0800

    update

--- src/main/java/com/wubaoguo/springboot/Swagger2.java
+++ src/main/java/com/wubaoguo/springboot/Swagger2.java
@@ -27,10 +27,10 @@ public class Swagger2 {
 	
 	private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
-                .title("Spring Boot中使用Swagger2构建RESTful APIs")
-                .description("更多Spring Boot相关文章请关注：http://blog.didispace.com/")
-                .termsOfServiceUrl("http://blog.didispace.com/")
-                .contact("程序猿DD")
+                .title("Spring Boot示例项目")
+                .description("更多java项目请关注：https://github.com/wustrive2008")
+                .termsOfServiceUrl("http://wubaoguo.com")
+                .contact("wustrive_2008")
                 .version("1.0")
                 .build();
     }
